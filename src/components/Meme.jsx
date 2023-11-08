414606ea9a12cbc1159a402de77064b21d54f1e5
Aviwekoli <aviwekoli@gmail.com>
2023-11-08 12:16:51 +0200

    Added Text on images and state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -21,23 +21,37 @@ const Meme = () => {
         }))
         
     }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input type="text" className="form--input" placeholder="Top text" name="topText" value={meme.topText} onChange={handleChange}/>
+                <input type="text" className="form--input" placeholder="Botton text" name="bottomText" value={meme.bottomText} onChange={handleChange}/>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
+};
+
+export default Meme;
+
+
     // const [ memeImage, setMemeImage ] = React.useState(""); 
     // const getMemeImage = () => {
     //     const { data: { memes: memesArray } } = memesData;
     //     const randomNumber = Math.floor(Math.random() * memesArray.length)
     //     setMemeImage(memesArray[randomNumber].url)
     // }
-    return (
-        <main>
-            <div className="form">
-                <input type="text" className="form--input" placeholder="Top text" />
-                <input type="text" className="form--input" placeholder="Botton text"/>
-                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
-            </div>
-            <img src={meme.randomImage} className="meme--image" />
-
-        </main>
-    )
-};
-
-export default Meme;
